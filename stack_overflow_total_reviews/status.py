7506891f2721bf88db2affa314acfa4d4c94e357
Taylor D. Edmiston <tedmiston@gmail.com>
2018-04-15 14:41:20 -0400

    Use json for status counts

--- stack_overflow_total_reviews/status.py
+++ stack_overflow_total_reviews/status.py
@@ -1,6 +1,13 @@
+import json
 from collections import OrderedDict
 
 from .models import get_queues
+
+
+def fetch_counts():
+    counts_json = '{"close": 9000, "reopen": 142, "suggested-edits": 98, "triage": 68, "helper": 40, "low-quality-posts": 37, "late-answers": 7, "first-posts": 30}'  # noqa
+    counts = json.loads(counts_json)
+    return counts
 
 
 def get_review_queues_current_status():
@@ -10,16 +17,7 @@ def get_review_queues_current_status():
     https://stackoverflow.com/review
     """
     queues = get_queues()
-    counts = {
-        'close': 9000,
-        'reopen': 142,
-        'suggested-edits': 98,
-        'triage': 68,
-        'helper': 40,
-        'low-quality-posts': 37,
-        'late-answers': 7,
-        'first-posts': 30,
-    }
+    counts = fetch_counts()
     queue_counts = OrderedDict([
         (queue, counts.get(queue.slug, 0)) for queue in queues])
     return queue_counts
