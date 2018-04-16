ed5bfc63b7a6b537c462a805eca18a9c057cc43c
Taylor D. Edmiston <tedmiston@gmail.com>
2018-04-15 22:00:34 -0400

    Add cache logging and improve cache logic

--- stack_overflow_total_reviews/cache.py
+++ stack_overflow_total_reviews/cache.py
@@ -1,4 +1,9 @@
+import logging
+
 import redis
+
+log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 DEFAULT_TTL = 86400
 
@@ -9,17 +14,22 @@ class Cache:
         self._redis = redis.StrictRedis(host='localhost', port=6379, db=0,
                                         decode_responses=True)
 
-    def set(self, key, val, ttl=DEFAULT_TTL):
+    def set_from_func(self, key, val_func, ttl=DEFAULT_TTL):
+        val = val_func()
         self._redis.set(key, val, ex=ttl)
 
     def get(self, key):
         return self._redis.get(key)
 
     def get_or_update(self, key, fetch_val, ttl=DEFAULT_TTL):
-        val = self._redis.get(key)
-        if not val:
-            self._redis.set(key, fetch_val(), ttl=ttl)
-            val = self._redis.get(key)
+        val = self.get(key)
+        if val is not None:
+            expires_in = self._redis.ttl(key)
+            log.debug(f'{key} cache hit (expires in {expires_in} seconds)')
+        else:
+            log.debug(f'{key} cache miss (fetching)')
+            self.set_from_func(key, fetch_val, ttl=ttl)
+            val = self.get(key)
         return val
 
 
