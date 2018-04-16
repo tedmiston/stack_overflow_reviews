af61c5f5c516963297fca0055efabecd2afddb93
Taylor D. Edmiston <tedmiston@gmail.com>
2018-04-15 22:30:34 -0400

    Tweak wording

--- stack_overflow_total_reviews/app.py
+++ stack_overflow_total_reviews/app.py
@@ -14,17 +14,14 @@ def main():
 
     total_reviews = sum(reviews.values())
 
-    print(f'You have performed {total_reviews} reviews on Stack Overflow as '
-          f'of {last_updated}.')
-
+    print(f'You\'ve performed {total_reviews} reviews as of {last_updated}:')
     for queue, count in sorted(reviews.items(),
                                key=lambda x: x[1], reverse=True):
         url = queue.get_stats_url()
         print(f'{count}\t{queue.name} ({url})')
 
     print()
-    print('-- Current queue status --')
-    for queue, count in get_review_queues_current_status(user).items():
+    print('Current queue status:')
     for queue, count in sorted(get_review_queues_current_status(user).items(),
                                key=lambda x: x[1], reverse=True):
         url = queue.get_review_url()
