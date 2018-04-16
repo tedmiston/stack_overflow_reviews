7b9e2ddca8753d4944deb1ab710610864d214a63
Taylor D. Edmiston <tedmiston@gmail.com>
2018-04-15 22:14:51 -0400

    Use json and cache for reviews

--- stack_overflow_total_reviews/stats.py
+++ stack_overflow_total_reviews/stats.py
@@ -1,20 +1,25 @@
+import json
 from collections import OrderedDict
+from datetime import datetime
 
+from .cache import cache
 from .models import get_queues
+
+
+def fetch_reviews():
+    counts = '{"suggested-edits": 6, "triage": 293, "helper": 0, "low-quality-posts": 0, "late-answers": 8, "first-posts": 21}'  # noqa
+    cache._redis.set('reviews_updated', datetime.now())
+    return counts
 
 
 def get_reviews_all_time():
     """Reviews given for queues I can access."""
-    queues = get_queues()
-    review_counts = {
-        'suggested-edits': 6,
-        'triage': 293,
-        'helper': 0,
-        'low-quality-posts': 0,
-        'late-answers': 8,
-        'first-posts': 21,
-    }
-    review_counts_all_queues = OrderedDict([
-        (queue, review_counts.get(queue.slug, 0)) for queue in queues])
-    last_updated = '2018-04-14'
-    return review_counts_all_queues, last_updated
+    reviews = cache.get_or_update('reviews', fetch_reviews, ttl=300)
+    reviews = json.loads(reviews)
+
+    reviews_updated = cache.get('reviews_updated')
+
+    reviews_all_queues = OrderedDict([(queue, reviews.get(queue.slug, 0))
+                                      for queue in get_queues()])
+
+    return reviews_all_queues, reviews_updated
